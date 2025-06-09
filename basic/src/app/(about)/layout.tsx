d546859c0579816a4878ee6bc2e39eca13ce522e
wcoder547 <mw5667155@gmail.com>
2025-06-09 19:20:14 +0500

    feat: add About, Team, and Service pages with layout integration

--- basic/src/app/(about)/layout.tsx
+++ basic/src/app/(about)/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +25,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>Hello layout</h1>
+        <h1>Hello About layout</h1>
         {children}
       </body>
     </html>
---
+++ basic/src/app/(about)/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>About section</div>;
+};
+
+export default Page;
