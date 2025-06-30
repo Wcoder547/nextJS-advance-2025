c252f2b44812157c9b0e930d2f322d4f4212c996
wcoder547 <mw5667155@gmail.com>
2025-06-30 10:33:33 +0500

    feat: implement database connection utility using Mongoose

---
+++ mstrymessage/src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to the database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(
+      (process.env.MONGODB_URI as string) || "",
+      {}
+    );
+    console.log(db.connections);
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("Connected to the database successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw new Error("Database connection failed");
+  }
+}
+
+export default dbConnect;
