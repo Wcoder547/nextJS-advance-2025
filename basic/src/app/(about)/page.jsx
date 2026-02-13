68587bee2789adf628dbc06dc367b1618488d039
WASEEM AKRAM <123148945+Wcoder547@users.noreply.github.com>
2026-02-13 02:46:03 -0800

    Delete basic directory

--- basic/src/app/(about)/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>About section</div>;
-};
-
-export default Page;
--- basic/src/app/service/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>Hello services</div>;
-};
-
-export default Page;
