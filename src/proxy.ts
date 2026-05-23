a0e550fae7ed88cfb6bc243337ccd8323912f1a6
WASEEM AKRAM <123148945+Wcoder547@users.noreply.github.com>
2026-05-23 16:14:09 +0500

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
 };
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
     (url.pathname.startsWith("/sign-in") ||
